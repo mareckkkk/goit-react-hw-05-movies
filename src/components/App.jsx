69770d1bb5470f590021059e812a42207cf4e29b
mareckkkk <m.walus94@gmail.com>
2024-07-09 09:18:19 +0200

    hw05

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,98 +1,26 @@
-import React, { Component } from "react";
-import { nanoid } from "nanoid";
-import { ContactForm } from "./ContactForm/ContactForm";
-import { Filter } from "./Filter/Filter";
-import { ContactList } from "./ContactList/ContactList";
+import { lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+import { SharedLayout } from "./SharedLayout/SharedLayout";
 
-export default class App extends Component {
-  state = {
-    contacts: [
-      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-    name: "",
-    number: "",
-  };
+const HomePage = lazy(() => import("../pages/HomePage"));
+const MovieDetailsPage = lazy(() => import("../pages/MovieDetailsPage"));
+const Movies = lazy(() => import("../pages/Movies"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const Cast = lazy(() => import("./Cast/Cast"));
+const Reviews = lazy(() => import("./Reviews/Reviews"));
 
-  componentDidMount() {
-    const contactsFromLocalStorage = localStorage.getItem("contactList");
-    const parsedContacts = JSON.parse(contactsFromLocalStorage);
-
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const prevStateContacts = prevState.contacts;
-    const nextStayContacts = this.state.contacts;
-
-    if (prevStateContacts !== nextStayContacts) {
-      localStorage.setItem("contactList", JSON.stringify(nextStayContacts));
-    }
-  }
-
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = (e) => {
-    const id = nanoid();
-    const name = e.name;
-    const number = e.number;
-    const contactsLists = [...this.state.contacts];
-
-    if (contactsLists.findIndex((contact) => name === contact.name) !== -1) {
-      alert(`${name} is already in contacts.`);
-    } else {
-      contactsLists.push({ name, id, number });
-    }
-
-    this.setState({ contacts: contactsLists });
-  };
-
-  handleDelete = (e) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((contact) => contact.id !== e),
-    }));
-  };
-
-  getFilteredContacts = () => {
-    const filterContactsList = this.state.contacts.filter((contact) => {
-      return contact.name
-        .toLowerCase()
-        .includes(this.state.filter.toLowerCase());
-    });
-
-    return filterContactsList;
-  };
-
-  render() {
-    const { filter } = this.state;
-
-    return (
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          marginLeft: 10,
-          alignItems: "left",
-          fontSize: 20,
-          color: "#010101",
-        }}>
-        <h1>Phonebook</h1>
-        <ContactForm handleSubmit={this.handleSubmit} />
-        <h2> Contacts</h2>
-        <Filter filter={filter} handleChange={this.handleChange} />
-        <ContactList
-          contacts={this.getFilteredContacts()}
-          handleDelete={this.handleDelete}
-        />
-      </div>
-    );
-  }
-}
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
